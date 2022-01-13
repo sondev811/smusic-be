b63a500caaeab5a5304052389b6fb2ad2e84f6d0
sondev <sondev811@gmail.com>
2022-01-13 16:54:45 +0700

    update cors

--- index.js
+++ index.js
@@ -8,15 +8,25 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-const options ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
+const corsOpts = {
+  origin: '*',
+  methods: [
+    'GET',
+    'POST',
+  ],
+  credentials:true,
    optionSuccessStatus:200,
 };
-app.use(cors(options));
+app.use(cors(corsOpts));
 app.use(bodyParser.json());
 connectDB();
-
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+})
 app.use('/api', routers);
 
-app.listen(PORT, () => console.log(`Runing on port ${PORT}`))
+app.listen(PORT, () => console.log(`Running on port ${PORT}`))
